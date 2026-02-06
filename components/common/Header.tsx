d98b39b3af1e5281ae22709881ab7dde45a73261
lKylelUndo <ando.kyle44@gmail.com>
2026-02-06 13:28:46 +0800

    add theme

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,18 +1,23 @@
 import Link from "next/link";
+import { ModeToggle } from "./ToggleTheme";
 
 const Header = () => {
   return (
     <header className="p-4">
       <div className="container mx-auto px-6 flex justify-between">
+        <div>
+          <h1 className="font-bold text-xl">PORTFOLIO</h1>
+        </div>
+
+        <nav className="flex items-center gap-x-6 text-[14px]">
+          <Link href={"/"}>Home</Link>
+          <Link href={"/about"}>About</Link>
+          <Link href={"/projects"}>Projects</Link>
+
           <div>
-            <h1 className="font-bold text-xl">PORTFOLIO</h1>
+            <ModeToggle />
           </div>
-    
-          <nav className="space-x-6 text-[14px]">
-            <Link href={"/"}>Home</Link>
-            <Link href={"/about"}>About</Link>
-            <Link href={"/projects"}>Projects</Link>
-          </nav>
+        </nav>
       </div>
     </header>
   );
