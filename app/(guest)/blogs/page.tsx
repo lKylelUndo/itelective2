d0f3195ed66adab6f9c8f90be3ae307558ea726d
lKylelUndo <ando.kyle44@gmail.com>
2026-02-13 13:10:50 +0800

    responsive

--- app/(guest)/blogs/page.tsx
+++ app/(guest)/blogs/page.tsx
@@ -6,9 +6,9 @@ export default function BlogsPage() {
  
   return (
     <section className="container px-6 mx-auto">
-      <h1 className="text-2xl font-semibold mt-4">Blog</h1>
+      <h1 className="text-2xl font-semibold mt-4 text-center">Latest Blog</h1>
 
-      <div className="p-5 grid grid-cols-3 gap-x-4">
+      <div className="p-5 grid grid-cols-1 lg:grid-cols-3 gap-x-4">
         {blogsObj.map((blog: BlogType) => (
             <div key={blog.id} className="bg-gray-50 rounded shadow space-y-4 p-4">
                 <span className="text-xs text-slate-600 font-semibold">{blog.blog_duration} mins read</span>
